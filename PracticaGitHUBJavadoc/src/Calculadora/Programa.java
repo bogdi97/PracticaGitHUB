29ef3e979115d7eb95706b7f02c720a2fe35925e
Bogdan <bogdan@MintEntornos.com>
2018-03-05 17:49:32 +0100

    Agregados nuevos metodos
    Agregados dos nuevos metodos a la calculadora, el primero realzia la
    potencia del segundo operador sobre el primero, y el segundo metodo
    imprime por pantalla la oracion "Calculadora de 'autor' y los dos
    operadores.

--- PracticaGitHUBJavadoc/src/Calculadora/Programa.java
+++ PracticaGitHUBJavadoc/src/Calculadora/Programa.java
@@ -2,10 +2,21 @@ package Calculadora;
 
 public class Programa {
 	
+	static Calculadora calc = new Calculadora(5,10);
+	
+	public static void main (String[] args) {
+		System.out.println(calc.toString());
+		System.out.println("Suma= "+calc.suma());
+		System.out.println("Resta= "+calc.resta());
+		System.out.println("Division= "+calc.division());
+		System.out.println("Potencia= "+calc.potencia());
+		
+	}
+	
 	//TODO: Crear un programa principal que 
 	//utilice todas las funciones de la calculadora
 	//Incluida las operaciones toString y la potencia
-	//que habréis añadido.
+	//que habrï¿½is aï¿½adido.
 	//Ejecutadlo.
 
 }
